a3c7b7437aecd0dba49c830898d71d241fccf102
apple <apple@apples-imac-6.attlocal.net>
2020-05-12 13:22:05 -0400

    Static key word

---
+++ Documents/eclipse-workspace/techproedsummerturkish2020/src/hazirlikdersi3/MyFirstClass.java
@@ -0,0 +1,18 @@
+package hazirlikdersi3;
+
+public class MyFirstClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");       //Cumle(Statement) bittiginde mutlaka noktali virgul(semi colon) kullan
+		                                          // ==> Tek satirlik comment icin kullanilir
+        System.out.println("Merhaba Dunya!");
+        
+        /*   
+               ==> Cok satirli comment ler icin kullanilir
+         */		
+	}
+
+}
+
+
